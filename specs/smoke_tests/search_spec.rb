64ca93fd6471e871e673881d0969c0a78726d0eb
Viacheslav Mefodin <mefodin.v@gmail.com>
2020-09-14 09:41:29 +0300

    Use %r for regexp in specs

--- specs/smoke_tests/search_spec.rb
+++ specs/smoke_tests/search_spec.rb
@@ -8,13 +8,13 @@ require 'yamlook'
 describe Yamlook::Search do
   describe '.perform' do
     it 'searches correctly for nested yaml keys' do
-      assert_output(/dummy\/test\.yml:5:8\nfoo/) do
+      assert_output(%r{dummy/test\.yml:5:8\nfoo}) do
         Yamlook::Search.perform(%w[test key])
       end
     end
 
     it 'searches correctly for dot-notated yaml keys' do
-      assert_output(/dummy\/test\.yaml:2:12\nbar/) do
+      assert_output(%r{dummy/test\.yaml:2:12\nbar}) do
         Yamlook::Search.perform(%w[wubba lubba dub dub])
       end
     end
