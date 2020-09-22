cf7a1de69f52f94d769f78f8bad3599d5ba64b15
Viacheslav Mefodin <mefodin.v@gmail.com>
2020-09-22 09:27:47 +0300

    Test for results quantity as well

--- spec/smoke_tests/search_spec.rb
+++ spec/smoke_tests/search_spec.rb
@@ -8,20 +8,34 @@ require 'yamlook'
 describe Yamlook::Search do
   describe '.perform' do
     it 'searches correctly for nested yaml keys' do
-      assert_output(%r{dummy/test\.yml:5:8\nfoo}) do
+      assert_output(%r{
+        Found\s1\soccurrences:\n
+        .+dummy/test\.yml:5:8\n
+        foo
+      }x) do
         Yamlook::Search.perform(%w[test key])
       end
     end
 
     it 'searches correctly for dot-notated yaml keys' do
-      assert_output(%r{dummy/test\.yaml:2:12\nbar}) do
+      assert_output(%r{
+        Found\s1\soccurrences:\n
+        .+dummy/test\.yaml:2:12\n
+        bar
+      }x) do
         Yamlook::Search.perform(%w[wubba lubba dub dub])
       end
     end
 
     it 'searches correctly for several occurrences' do
       assert_output(
-        %r{dummy/test\.yml:7:14\ntrue\n\n.+dummy/test\.yaml:4:14\ntrue}
+        %r{
+          Found\s2\soccurrences:\n
+          .+dummy/test\.yml:7:14\n
+          true\n\n
+          .+dummy/test\.yaml:4:14\n
+          true
+        }x
       ) do
         Yamlook::Search.perform(%w[test duplicate])
       end
